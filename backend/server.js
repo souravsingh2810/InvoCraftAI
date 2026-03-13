dd5a5a0d6c1573708681139996a20afb44c4bbc3
sourav <souravs2810@gmail.com>
2026-03-13 22:24:56 +0530

    cors policy updation

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 4000;
 app.use(cors({
     origin: [
     "http://localhost:5173",
-    // "https://vyapaar-ai-nine.vercel.app"
+    "https://invo-craft-ai.vercel.app"
   ],
 }));
 // app.use(cors());
